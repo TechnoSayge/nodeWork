63089cec6cfa2aff40bdbee2c74bcd5c12f7bb6f
Yolanda Charles <technosayge@gmail.com>
2019-01-08 14:35:23 -0500

    adding functionality for Middleware

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-function rqListener(req, res) {
+const app = express();
+/*
+app.use((req, res, next) => {
+    console.log('In the middleware!'); 
+    next(); //Allows the request to continue to the next middleware line...
+});
+*/
+app.use((req, res, next) => {
+    console.log('In another middleware!')
+    res.send('<h1>Hello from Express!</h1>');
+});
 
-}
-
-http.createServer();
+app.listen(3000);
