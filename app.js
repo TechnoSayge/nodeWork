e8e839be94a12facfd3626bf5dc5b24a4efffcfa
Yolanda Charles <technosayge@gmail.com>
2019-01-11 12:56:05 -0500

    Added html and styling

--- app.js
+++ app.js
@@ -1,48 +1,21 @@
-/*
-const express = require('express');
+const path = require('path');
 
-const app = express();
-
-app.use((req, res, next) => {
-    console.log('In the middleware!'); 
-    next(); //Allows the request to continue to the next middleware line...
-});
-
-app.use((req, res, next) => {
-    console.log('In another middleware!')
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-app.listen(3000);
-*/
-/*************************************************************************** */
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express(); 
-/*Parsing of the body*/
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-/*Begins Route Handling Middleware*/
-//visit expressjs.com to learn more about .use (api reference tab)
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-app.use('/add-product', (req, res, next) => {
-	console.log('Middleware 2!');
-	res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-app.use('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-	console.log('Middleware 3!');
-	res.send('<h1>But of course!</h1>');
-}); /*Ends Route Handling Middleware */
 
 app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html')); //global variable that holds our absolute path; path join automatically detects your OS.
+});
+
+module.exports = router;
